9ee3b34d3733cd0a6556a197841b263a855e2a27
rifdanisyt <115394191+rifdanisyt@users.noreply.github.com>
2022-10-23 10:50:08 +0700

    Add files via upload

---
+++ tombol_oneklik/tombol_oneklik.ino
@@ -0,0 +1,26 @@
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+#include <Dabble.h>
+bool pernahstart=0;
+void setup() {
+  Serial.begin(9600);
+  Dabble.begin(9600);
+}
+
+void loop() {
+  Dabble.processInput();         
+  Serial.print("KeyPressed: ");
+  bool start=GamePad.isStartPressed();
+  if(start){
+    if(pernahstart){
+      start=0;
+    }
+    else{
+      pernahstart=1;
+    }
+  }
+  else{
+    pernahstart=0;
+  }
+  if(start) serial.println("Start");
+}
